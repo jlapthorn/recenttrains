5a653fab143f0f3f729c826da43983d9363be44f
James Lapthorn <james.lapthorn@lapthornconsulting.com>
2018-05-17 09:38:18 +0100

    Update train.py

--- train.py
+++ train.py
@@ -12,19 +12,21 @@ def pushbulletinit(msg):
     pb = Pushbullet("o.5V4FfVDdrdhvDsuxFH0Hmt7M5enT5bAq")
     push = pb.push_note("SWR Late Trains Alert", msg)
 
-r = requests.get(url) # Get The URL
-soup = BeautifulSoup(r.text, 'html.parser') # Parse the HTML as a string
-trainTimes = soup.find_all('table')[1] # Find the second table containing train times
+def main():
+    r = requests.get(url) # Get The URL
+    soup = BeautifulSoup(r.text, 'html.parser') # Parse the HTML as a string
+    trainTimes = soup.find_all('table')[1] # Find the second table containing train times
 
-for table_row in trainTimes.select("tr"):
-    cells = table_row.findAll('td')
-    if len(cells) > 0 and len(cells[6]) > 0:
-        leave=cells[1].text.strip()
-        arrival=cells[2].text.strip()
-        time,status=cells[6].text.strip().split(" ")
-        if "L" in status: 
-            minutes=int(status.replace('L',''))
-            if minutes > late:
-                msg = "The train leaving Winnersh at {0} was more than {3} minutes late.  It was {1} minutes late in total arriving at {2}".format(leave, str(minutes),arrival,str(late))   
-                pushbulletinit(msg)
+    for table_row in trainTimes.select("tr"):
+        cells = table_row.findAll('td')
+        if len(cells) > 0 and len(cells[6]) > 0:
+            leave=cells[1].text.strip()
+            arrival=cells[2].text.strip()
+            time,status=cells[6].text.strip().split(" ")
+            if "L" in status: 
+                minutes=int(status.replace('L',''))
+                if minutes > late:
+                    msg = "The train leaving Winnersh at {0} was more than {3} minutes late.  It was {1} minutes late in total arriving at {2}".format(leave, str(minutes),arrival,str(late))   
+                    pushbulletinit(msg)
 
+     main()
