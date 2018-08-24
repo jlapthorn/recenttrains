843e3c47deac1eb618d5f33e8d0c2c930c436e05
James Lapthorn <james.lapthorn@lapthornconsulting.com>
2018-08-24 15:25:14 +0100

    Delete common.py

--- common.py
+++
@@ -1,8 +0,0 @@
-debug=True
-amURL="http://www.recenttraintimes.co.uk/Home/Search?Op=Srch&Fr=Winnersh+%28WNS%29&To=London+Waterloo+%28WAT%29&TimTyp=D&TimDay=4a&Days=Wk&TimPer=4w&dtFr=&dtTo=&ShwTim=AvAr&TOC=All&ArrSta=5&MetAvg=Mea&MetSpr=RT&MxScDu=&MxSvAg=&MnScCt=&MxArCl=5"
-pmURL="http://www.recenttraintimes.co.uk/Home/Search?Op=Srch&Fr=London+Waterloo+%28WAT%29&To=Winnersh+%28WNS%29&TimTyp=D&TimDay=4p&Days=Wk&TimPer=4w&dtFr=&dtTo=&ShwTim=AvAr&TOC=All&ArrSta=5&MetAvg=Mea&MetSpr=RT&MxScDu=&MxSvAg=&MnScCt=&MxArCl=5"
-pbSecret="o.5V4FfVDdrdhvDsuxFH0Hmt7M5enT5bAq"
-lateTime=15
-pmHour=17
-#telegramToken='529545025:AAHv9kjpraq6XEHQ6HRYKA3BgTAJsxTO6UM'
-telegramToken='637498030:AAF1Pzn7E4_xVYQwq-4mN2AHgK4joP3KbFw'
